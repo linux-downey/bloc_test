913b255e959850de72bfe50c304bde40aa6a2011
linux-downey <1148256554@qq.com>
2018-09-10 05:41:49 -0700

    python modify is not to !=

--- leetcode/two_sum/two_sum.py
+++ leetcode/two_sum/two_sum.py
@@ -1,16 +1,18 @@
 def twoSum(nums, target):
         data=[]
+        print len(nums)
         for i in range(len(nums)):
+            print len(data)
             for j in range(len(data)):
-                if(nums[i] is not data[j]):
-                    pass
+                if(nums[i] != data[j]):
+                    print 12
                 else:
                     return [j,i]
             data.append(target-nums[i])
         return None
 def main():
-    nums = [2,7,11,15]
-    target =17
+    nums = [22222,22222]
+    target =44444
     result=twoSum(nums,target)
     print result
 
