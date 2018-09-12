34954ef354cb64323b9a0aea7c717cfd383c8437
linux-downey <1148256554@qq.com>
2018-09-12 08:42:10 -0700

    recursive way reverse integer

--- leetcode/reverse_interger/reverse_integer.cpp
+++ leetcode/reverse_interger/reverse_integer.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 
 using namespace std;
-int reverse_num(int x)
+int reverse_num(int x,int &res)
 {
-	int val=0;
-	while(x)
-	{
-		val=val*10+x%10;
-		x=x/10;
-	}
-	return val;
+	if(!x) return res;
+	res=res*10+x%10;
+	return reverse_num(x/10,res);
 }
 
 
 
 int main()
 {
-	int val=reverse_num(-3245454);
+	int val=0;
+	val=reverse_num(-3245454,val);
 	cout<<val<<endl;
 }
