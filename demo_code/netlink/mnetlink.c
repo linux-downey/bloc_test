812cf57f6390990b2f85651a74c8ce6ebb25beaf
downeyboy <ddowneyboy@gmail.com>
2020-07-09 23:23:08 +0800

    ad

---
+++ demo_code/netlink/mnetlink.c
@@ -0,0 +1,69 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/kobject.h>
+#include <linux/sysfs.h>
+#include <linux/slab.h>
+#include <net/netlink.h>
+#include <net/sock.h>
+
+#define debug  true
+MODULE_LICENSE("GPL");              
+
+#define MY_NETLINK 28
+struct sock *sock = NULL;
+
+static void my_netlink_rcv(struct sk_buff *skb)
+{
+	struct nlmsghdr *nlh = NULL;
+	int ret = 0;
+	char *data = NULL;
+	char *back_str = "How are you,downey!";
+
+	nlh = nlmsg_hdr(skb);
+	data = NLMSG_DATA(nlh);
+	pr_info("Recv data = %s\n",data);
+
+	int len = strlen(back_str);	
+
+	struct sk_buff *send_skb;
+	send_skb = alloc_skb(len,GFP_KERNEL);
+	if(send_skb){
+		char *scratch;
+		scratch = skb_put(send_skb, len);
+		sprintf(scratch, "%s",back_str);
+		NETLINK_CB(send_skb).dst_group = 1;
+//		ret = netlink_broadcast(sock,send_skb,100,1,GFP_KERNEL);
+		ret = netlink_unicast(sock,send_skb,100,GFP_KERNEL);
+		if(ret == -ENOBUFS || ret == -ESRCH){
+			pr_err("Failed to broadcast,ret = %d\n",ret);
+		}
+	}
+
+}
+
+static int __init mnetlink_init(void)
+{
+	int ret = 0;
+	struct netlink_kernel_cfg cfg = {
+		.groups = 1,
+		.input	= my_netlink_rcv,
+		/* whether the non-root user can recv/send broadcast */
+		.flags  = NL_CFG_F_NONROOT_RECV | NL_CFG_F_NONROOT_SEND,
+	};
+	pr_info("Create kernel netlink\n");
+	sock = netlink_kernel_create(&init_net, MY_NETLINK, &cfg);
+	if(NULL == sock){
+		pr_err("Failed to create socket\n");
+	}
+	return 0;
+}
+
+static void __exit mnetlink_exit(void)
+{
+	pr_info("Release kernel netlink\n");
+	netlink_kernel_release(sock);
+}
+
+module_init(mnetlink_init);
+module_exit(mnetlink_exit);
---
+++ demo_code/netlink/netlink_server.c
@@ -0,0 +1,93 @@
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <asm/types.h>
+#include <linux/netlink.h>
+#include <linux/socket.h>
+#include <errno.h>
+
+#define  MSGLEN 512 
+#define  SRC_PID  100
+#define  DST_PID  101
+
+#define MY_NETLINK 28
+
+int init_netlink(void)
+{
+	int ret = 0;
+	int nlfd = 0;
+	struct nlmsghdr *nlhdr = NULL;
+	struct sockaddr_nl src_addr,dest_addr;
+	struct msghdr msg;
+	int state;
+	
+	nlfd = socket(AF_NETLINK, SOCK_RAW, MY_NETLINK);
+	if(nlfd < 1) return -1;
+	
+	memset(&src_addr,0,sizeof(src_addr));
+	memset(&dest_addr,0,sizeof(dest_addr));
+	
+	src_addr.nl_family = AF_NETLINK;
+	src_addr.nl_pid = SRC_PID;
+	src_addr.nl_groups = 1;
+		
+	ret = bind(nlfd, (struct sockaddr *)&src_addr,
+                sizeof(src_addr));
+	if(ret < 0 ) {
+		close(nlfd);
+		return -2;
+	}
+	
+	nlhdr = malloc(NLMSG_SPACE(MSGLEN));
+	if(!nlhdr) {
+		close(nlhdr);
+		return -3;
+	}
+	
+	struct iovec iov;
+	int state_smg = 0;
+
+	iov.iov_base = (void *)nlhdr;
+	iov.iov_len = NLMSG_SPACE(MSGLEN);
+	memset(&msg, 0, sizeof(msg));
+	
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+	memset(nlhdr,0,NLMSG_SPACE(MSGLEN));
+
+	while(1){
+		state = recvmsg(nlfd, &msg, 0);
+		if(state < 0){
+
+		}
+		else{
+			
+			printf("recieve message:%s\n",(char *) NLMSG_DATA(nlhdr));
+			//struct sockaddr_nl *dst  = (struct sockaddr_nl *)msg.msg_name;
+			printf("recv pid :%d\n",nlhdr->nlmsg_pid);
+			printf("recv pid :%d\n",nlhdr->nlmsg_len);
+			printf("recv pid :%d\n",nlhdr->nlmsg_type);
+			/*
+			dest_addr.nl_family = AF_NETLINK;
+			dest_addr.nl_pid = nlhdr->nlmsg_pid; // 0 - kernel
+			dest_addr.nl_groups = 0;
+			*/
+		}
+	}
+	return 0;
+}
+
+
+
+int main(void)
+{
+	int ret = 0;
+	ret = init_netlink();
+	printf("ret = %d\n",ret);
+	return 1;
+}
+
